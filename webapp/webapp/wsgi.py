6fa587a9814617b112a853483b55d7f55f67843d
Alejandro <mapachonpa@gmail.com>
2019-02-04 18:31:34 -0500

    Organización de carpetas

--- webapp/webapp/wsgi.py
+++ webapp/webapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webBD project.
+WSGI config for webapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webBD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 
 application = get_wsgi_application()
